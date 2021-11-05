cd923e2842e4f08631a6406361803feceaa6b365
kellogsricekrispy <79350471+kellogsricekrispy@users.noreply.github.com>
2021-11-05 10:11:52 -0400

    changed cursor

--- docs/javascript/main.js
+++ docs/javascript/main.js
@@ -15,9 +15,15 @@ $(document).ready(()=>{
         }
     }
 
-    window.addEventListener(mousemove, (e)=>{
+    window.addEventListener('mousemove', (e)=>{
         let mouseX = e.pageX;
         let mouseY = e.pageY;
+        // console.log(mouseX, mouseY);
+        $('.dot').css({
+            left: mouseX - 15,
+            top: mouseY - 15,
+            // top: mouseY - $('body').scrollTop(),
+        })
         
     })
 })
